219ca5674de29eedfe2ba2eaf4684af1f873cdb1
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2020-12-04 14:22:30 +0530

    Code Updated for mult

--- src/index.js
+++ src/index.js
@@ -12,36 +12,34 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
-
-app.get('/',(req,res)=>{
-    res.send("Hellow world");
+app.get("/", (req, res)=>{
+    res.send("Hello world!");
 });
 
-app.post("/add",(req, res) =>{
+app.post("/add", (req, res)=>{
     if((typeof(req.body.num1) === 'string' || typeof(req.body.num2) === 'string')){
         res.send({
             message: "Invalid data types"
         })
-    } else{
-        let num1=req.body.num1;
-        let num2=req.body.num2;
-        result=num1+num2;
-        if(result<-1000000 || num1<-1000000 || num2<-1000000){
+    }else{
+        let num1 = req.body.num1;
+        let num2 = req.body.num2;
+        result = num1 + num2;
+        if(result < -1000000 || num1 < -1000000 || num2 < -1000000){
             res.send({
-                message: "underflow"
+                message: "Underflow"
             })
-        } else if(result >1000000 || num1 >1000000 || num2 >1000000){
+        }else if(result > 1000000 || num1 > 100000 || num2 > 100000){
             res.send({
-                message:"Overflow"
+                message: "Overflow"
             })
-        } else{
+        }else{
             res.send({
-                status:"success",
-                message: "The Sum of two given number",
-                sum:result
-            });
+                status: "success",
+                message: "the sum of given two numbers",
+                sum: result
+                });
         }
-        l
     }
 });
 
@@ -72,7 +70,7 @@ app.post("/sub", (req, res)=>{
     }
 });
 
-app.post("/multiplication", (req, res)=>{
+app.post("/multiply", (req, res)=>{
     if((typeof(req.body.num1) === 'string' || typeof(req.body.num2) === 'string')){
         res.send({
             message: "Invalid data types"
@@ -80,7 +78,7 @@ app.post("/multiplication", (req, res)=>{
     }else{
         let num1 = req.body.num1;
         let num2 = req.body.num2;
-        result = num1 - num2;
+        result = num1 * num2;
         if(result < -1000000 || num1 < -1000000 || num2 < -1000000){
             res.send({
                 message: "Underflow"
@@ -92,14 +90,14 @@ app.post("/multiplication", (req, res)=>{
         }else{
             res.send({
                 status: "success",
-                message: "The multiplication of given two numbers",
-                difference: result
+                message: "The product of given numbers",
+                result: result
                 });
         }
     }
 });
 
-app.post("/division", (req, res)=>{
+app.post("/divide", (req, res)=>{
     if((typeof(req.body.num1) === 'string' || typeof(req.body.num2) === 'string')){
         res.send({
             message: "Invalid data types"
@@ -107,7 +105,13 @@ app.post("/division", (req, res)=>{
     }else{
         let num1 = req.body.num1;
         let num2 = req.body.num2;
-        result = num1 - num2;
+        if(num2 === 0){
+            res.send({
+                status: "error",
+                message: "Cannot divide by zero"
+            })
+        }
+        result = num1 / num2;
         if(result < -1000000 || num1 < -1000000 || num2 < -1000000){
             res.send({
                 message: "Underflow"
@@ -119,8 +123,8 @@ app.post("/division", (req, res)=>{
         }else{
             res.send({
                 status: "success",
-                message: "The division of given two numbers",
-                difference: result
+                message: "The division of given numbers",
+                result: result
                 });
         }
     }
